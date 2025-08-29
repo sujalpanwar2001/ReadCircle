c67499831160d4b86c6c035f8c7425dbd0e71668
Sujal Panwar <sujalpanwar2001@gmail.com>
2025-08-29 10:43:21 +0530

    passing github secrets for email for the unit testing

--- readcircle/src/test/java/com/sujal/readcircle/ReadcircleApplicationTests.java
+++ readcircle/src/test/java/com/sujal/readcircle/ReadcircleApplicationTests.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.mail.MailSenderAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@EnableAutoConfiguration(exclude = {MailSenderAutoConfiguration.class})
 class ReadcircleApplicationTests {
 
 	@Test
