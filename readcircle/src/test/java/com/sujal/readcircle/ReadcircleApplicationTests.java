1baf71a8a31692da799c9abbf651f736bea32da0
Sujal Panwar <sujalpanwar2001@gmail.com>
2025-08-29 11:11:41 +0530

    mocking beans and other services for the context for testing

--- readcircle/src/test/java/com/sujal/readcircle/ReadcircleApplicationTests.java
+++ readcircle/src/test/java/com/sujal/readcircle/ReadcircleApplicationTests.java
@@ -1,13 +1,39 @@
 package com.sujal.readcircle;
 
+import com.sujal.readcircle.auth.AuthenticationService;
+import com.sujal.readcircle.book.BookService;
+import com.sujal.readcircle.file.FileStorageService;
+import com.sujal.readcircle.security.JwtService;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.mail.MailSenderAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(classes = {}) // Empty, so no beans are loaded
+@SpringBootTest
+@ActiveProfiles("test")
 class ReadcircleApplicationTests {
+    @MockBean
+    private AuthenticationService authenticationService;
+
+    @MockBean
+    private JwtService jwtService;
+
+    @MockBean
+    private BookService bookService;
+
+    @MockBean
+    private FileStorageService fileStorageService;
+
+    @MockBean
+    private JavaMailSender javaMailSender;
+
+
 
     @Test
-    void contextLoads() {
-        // This just checks that the test itself runs
-    }
+	void contextLoads() {
+	}
+
 }
