35aa88993f8a7064e9639eb62054832e268b75e7
Sujal Panwar <sujalpanwar2001@gmail.com>
2025-08-29 13:21:34 +0530

    FE pipeline fix

--- readcircle-ui/src/app/app.module.ts
+++ readcircle-ui/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { RegisterComponent } from './pages/register/register.component';
 import { ActivateAccountComponent } from './pages/activate-account/activate-account.component';
 import { CodeInputModule } from 'angular-code-input';
 import { HttpTokenInterceptor } from './services/interceptor/http-token.interceptor';
+import { ApiModule } from './services/api.module';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { HttpTokenInterceptor } from './services/interceptor/http-token.intercep
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-CodeInputModule
+    CodeInputModule,
+    ApiModule.forRoot({rootUrl: 'http://141.148.222.231:8088/api/v1'})
   ],
   providers: [
     HttpClient,
