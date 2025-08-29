5bbd8b32071c1792f68985eec335847fb5faaeaa
Sujal Panwar <sujalpanwar2001@gmail.com>
2025-08-29 10:36:45 +0530

    fixing the mail issue for tests

--- readcircle/src/test/java/com/sujal/readcircle/ReadcircleApplicationTests.java
+++ readcircle/src/test/java/com/sujal/readcircle/ReadcircleApplicationTests.java
@@ -1,9 +1,12 @@
 package com.sujal.readcircle;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.mail.MailSenderAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {MailSenderAutoConfiguration.class})
 class ReadcircleApplicationTests {
 
 	@Test
