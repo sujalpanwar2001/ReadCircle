307ec1d2e8aa9090814f22a0d79d0af0fb7c9823
Sujal Panwar <sujalpanwar2001@gmail.com>
2025-08-29 10:21:38 +0530

    Setting up the backend pipeline and testing it on the Oracle Cloud infra vps

--- readcircle/src/main/java/com/sujal/readcircle/config/BeansConfig.java
+++ readcircle/src/main/java/com/sujal/readcircle/config/BeansConfig.java
@@ -1,6 +1,7 @@
 package com.sujal.readcircle.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
@@ -25,6 +26,9 @@ import java.util.List;
 public class BeansConfig {
 
     private final UserDetailsService userDetailsService;
+
+    @Value("${application.cors.origins:*}")
+    private List<String> allowedOrigin;
 
     @Bean
     public AuthenticationProvider authenticationProvider(UserDetailsService userDetailsService){
@@ -54,21 +58,10 @@ public class BeansConfig {
     public CorsFilter corsFilter() {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:4200","http://localhost:8080"));
-        config.setAllowedHeaders(Arrays.asList(
-                HttpHeaders.ORIGIN,
-                HttpHeaders.CONTENT_TYPE,
-                HttpHeaders.ACCEPT,
-                HttpHeaders.AUTHORIZATION
-        ));
-        config.setAllowedMethods(Arrays.asList(
-                "GET",
-                "POST",
-                "DELETE",
-                "PUT",
-                "PATCH"
-        ));
+        // config.setAllowCredentials(true);
+        config.setAllowedOrigins(allowedOrigin);
+        config.setAllowedHeaders(Arrays.asList("*"));
+        config.setAllowedMethods(Arrays.asList("*"));
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
 
