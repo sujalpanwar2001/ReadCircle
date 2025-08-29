58a7414ff0cc356e172985ff939af1283f8edd4f
Sujal Panwar <sujalpanwar2001@gmail.com>
2025-08-29 10:58:10 +0530

    testing for testing

--- readcircle/src/test/java/com/sujal/readcircle/ReadcircleApplicationTests.java
+++ readcircle/src/test/java/com/sujal/readcircle/ReadcircleApplicationTests.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.mail.MailSenderAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ReadcircleApplicationTests {
 
 	@Test
