153af756a08dc6de792b40fdae0260f564a2e69b
Sujal Panwar <sujalpanwar2001@gmail.com>
2025-09-09 09:22:06 +0530

    testing unit tests fix 3

--- readcircle/src/test/java/com/sujal/readcircle/ReadcircleApplicationTests.java
+++ readcircle/src/test/java/com/sujal/readcircle/ReadcircleApplicationTests.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.mail.MailSenderAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.ActiveProfiles;
